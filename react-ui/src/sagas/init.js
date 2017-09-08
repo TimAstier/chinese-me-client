f335ed0a1a0ad5b123dabe2db3ae60a82f2af64c
Tim <timothee.astier@essec.edu>
2017-09-08 14:43:30 +0200

    Fix bug to allow initApp after login

--- react-ui/src/sagas/init.js
+++ react-ui/src/sagas/init.js
@@ -2,9 +2,11 @@ import { take, call } from 'redux-saga/effects';
 import { types as sagaTypes } from './actions';
 import { fetchEntities } from './entities';
 
-// This is called only one time, when App containers mounts
+// This is called only one time, when Study containers mounts
 export default function* initApp() {
-  yield take(sagaTypes.INIT_APP);
-  yield call(fetchEntities, '/seasons');
-  yield call(fetchEntities, '/episodes');
+  while (true) { // eslint-disable-line no-constant-condition
+    yield take(sagaTypes.INIT_APP);
+    yield call(fetchEntities, '/seasons');
+    yield call(fetchEntities, '/episodes');
+  }
 }

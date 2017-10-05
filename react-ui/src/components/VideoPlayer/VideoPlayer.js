4a58c5bdafc709e40ab17ba25d75af7d0dc6b7d1
Tim <timothee.astier@essec.edu>
2017-10-06 00:32:40 +0200

    Pause videos on spacebar and go fullscreen on dblclick

--- react-ui/src/components/VideoPlayer/VideoPlayer.js
+++ react-ui/src/components/VideoPlayer/VideoPlayer.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
 
+let videoRef;
+
 class VideoPlayer extends Component {
+  constructor() {
+    super();
+    this.handleEnterKeyPress = this.handleEnterKeyPress.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleEnterKeyPress);
+    document.addEventListener('dblclick', this.handleDblClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleEnterKeyPress);
+    document.removeEventListener('dblclick', this.handleDblClick);
+  }
+
+  handleEnterKeyPress(event) {
+    if (event.keyCode === 32) {
+      return videoRef.paused ? videoRef.play() : videoRef.pause();
+    }
+    return null;
+  }
+
+  handleDblClick() {
+    if (videoRef.requestFullscreen) {
+      videoRef.requestFullscreen();
+    } else if (videoRef.mozRequestFullScreen) {
+      videoRef.mozRequestFullScreen();
+    } else if (videoRef.webkitRequestFullscreen) {
+      videoRef.webkitRequestFullscreen();
+    }
+  }
 
   render() {
     return (
       <video
+        ref={video => {videoRef = video;}}
         width={this.props.width}
         height={this.props.height}
         controls

2bc69fd97db410f3923fc336851b7f11dcb01d00
Tim <timothee.astier@essec.edu>
2017-10-19 12:21:29 +0200

    Fix ScreenWrapper that was causing children to be re-constructed on every EpisodeScreen render

--- react-ui/src/components/ScreenWrapper/ScreenWrapper.js
+++ react-ui/src/components/ScreenWrapper/ScreenWrapper.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import propTypes from 'prop-types';
 import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  margin-top: 30px;
+  width: 1200px;
+  height: 640px;
+  border-radius: 10px;
+  background-color: #ffffff;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.08);
+  display: flex;
+  flex-direction: column;
+`;
+
 class ScreenWrapper extends Component {
 
   render() {
-    const Wrapper = styled.div`
-      margin-top: 30px;
-      width: 1200px;
-      height: 640px;
-      border-radius: 10px;
-      background-color: #ffffff;
-      box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.08);
-      display: flex;
-      flex-direction: column;
-    `;
-
     return (
       <Wrapper>{this.props.children}</Wrapper>
     );

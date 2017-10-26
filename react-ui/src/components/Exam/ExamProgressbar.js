c222463392a112c28c4fa5ec447f930767781e43
Tim <timothee.astier@essec.edu>
2017-10-26 17:42:38 +0200

    Add react-motion and animate exam score

--- react-ui/src/components/Exam/ExamProgressbar.js
+++ react-ui/src/components/Exam/ExamProgressbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
 import styled from 'styled-components';
+import { Motion, spring } from 'react-motion';
 
 const Wrapper = styled.div`
   width: 20px;
@@ -22,24 +23,32 @@ const Score = styled.div`
     ${props => props.colorA},
     ${props => props.colorB}
     )
-  }
+  };
   box-shadow: ${props => props.abovePass ?
     '0 2px 8px 0 #91ca49'
     : '0 2px 8px 0 #55b6ff'
-  }
+  };
 `;
 
 class ExamProgressbar extends Component {
 
   render() {
+    const height = this.props.score / this.props.scoreMax * 320;
     return (
       <Wrapper>
-        <Score
-          height={this.props.score / this.props.scoreMax * 320}
-          abovePass={this.props.score >= 7}
-          colorA={this.props.score >= 7 ? '#9fdb53' : '#8edcff'}
-          colorB={this.props.score >= 7 ? '#91ca49' : '#55b6ff'}
-        />
+        <Motion
+          defaultStyle={{ y: 0 }}
+          style={{ y: spring(height, { stiffness: 180, damping: 12 }) }}
+        >
+          { ({ y }) => (
+            <Score
+              height={y}
+              abovePass={this.props.score >= 7}
+              colorA={this.props.score >= 7 ? '#9fdb53' : '#8edcff'}
+              colorB={this.props.score >= 7 ? '#91ca49' : '#55b6ff'}
+            />
+          )}
+        </Motion>
       </Wrapper>
     );
   }

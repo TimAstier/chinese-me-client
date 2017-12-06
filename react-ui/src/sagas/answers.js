de99d012c5c210a06f352bbb2b31c86d679ca23c
Tim <timothee.astier@essec.edu>
2017-12-06 14:05:40 +0100

    Minor cleaning

--- react-ui/src/sagas/answers.js
+++ react-ui/src/sagas/answers.js
@@ -1,17 +1,14 @@
 import { take, call } from 'redux-saga/effects';
 import Api from '../utils/api';
-import { types as sagaTypes, actions as sagaActions } from './actions';
+import { types as sagaTypes } from './actions';
 
 export function* watchSaveAnswer() {
   while (true) { // eslint-disable-line no-constant-condition
     const action = yield take(sagaTypes.SAVE_ANSWER);
     try {
-      const response = yield call(Api.post, '/answers', action.payload.answer);
-      console.log(response);
+      yield call(Api.post, '/answers', action.payload.answer);
     } catch (error) {
       // TODO: handle error
-      // yield call(reject, new SubmissionError(error.response.data));
-      // console.log('User creation failed');
     }
   }
 }

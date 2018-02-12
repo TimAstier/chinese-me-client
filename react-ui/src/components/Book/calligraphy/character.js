1fd5ef736e10892a293fd17329d693644b8fd693
Tim <timothee.astier@essec.edu>
2018-02-12 10:52:36 +0100

    Fix typo and cookie consent message

--- react-ui/src/components/Book/calligraphy/character.js
+++ react-ui/src/components/Book/calligraphy/character.js
@@ -11,7 +11,7 @@ export default class Content extends Component {
           If we compare the roof radical in <c.Chinese>字</c.Chinese> and <c.Chinese>客</c.Chinese>, we can see that they vary in breadth: the roof on <c.Chinese>字</c.Chinese> spreads out horizontally, while the roof on <c.Chinese>客</c.Chinese> is shorter. This is due to the difference in the lower part of each character. The bottom part of <c.Chinese>字</c.Chinese> is narrow with few strokes; it needs a big <i>roof</i> to balance the whole. The bottom of <c.Chinese>客</c.Chinese> is more dense, with a long sweeping right downward stroke, so the <i>roof</i> becomes more of a dainty decoration on top of the character’s sturdy foundation.
         </c.P>
         <c.Bookrow>
-          <c.CharacterBox simpChar="语" />
+          <c.CharacterBox simpChar="字" />
         </c.Bookrow>
       </div>
     );

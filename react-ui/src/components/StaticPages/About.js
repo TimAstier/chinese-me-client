a67fa3c96cfdecc46e11659226a0ec1852217e1e
Tim <timothee.astier@essec.edu>
2018-04-17 15:10:02 +0200

    Update about page

--- react-ui/src/components/StaticPages/About.js
+++ react-ui/src/components/StaticPages/About.js
@@ -24,15 +24,15 @@ class Privacy extends Component {
         <c.Page>
           <c.Bookrow marginBottom={30} center>
             <H1>
-              <Title>Chinese isn’t difficult - just different!</Title>
+              <Title>About ChineseMe</Title>
             </H1>
           </c.Bookrow>
-          <c.PartTitle type="secondary">Welcome to ChineseMe!</c.PartTitle>
-          <c.P>Chinese has a reputation for being difficult. We believe this is mainly because the language is so different from what we are used to: pronunciation, vocabulary, writing system, grammar and cultural context can all seem unfamiliar.</c.P>
-          <c.P>ChineseMe is an integrated language course that specifically addresses these differences. <i>The Basics</i> is a 10-lesson introduction course for total beginners. <i>Season 1</i> is a mini soap opera soon to be released. Both courses will be available free of charge as printable pdf’s.</c.P>
-          <c.P>In this beta release, interactive features such as sound recordings, exercises, calligraphy videos and character history are also free - but they will soon become a USD 19.95 monthly subscription feature. The first few lessons of <i>The Basics</i> will remain free, though.</c.P>
-          <c.P>The beta release has been tested for the browsers Chrome, Firefox and Safari on PC and Mac, as well as for Explorer on PC. It currently does not work smoothly on phones and tablets.</c.P>
-          <c.P>Click <a href="/study/philosophy">here</a> to learn more about the philosophy behind ChineseMe.</c.P>
+          <c.P>ChineseMe is an <a href="https://blog.chinese-me.com/a-chinese-course-where-you-are-the-star-aac9e21c1c49">integrated Chinese course</a> designed for committed learners.</c.P>
+          <c.P>Our aim is to combine the best ideas for pronunciation, vocabulary, characters, sentence patterns (grammar), cultural knowledge and interactive exercises in a single, integrated learning experience. The course has the form of a systematic textbook with rich content available under interactive icons: sound, HD videos of calligraphy, character etymology and interactive exercises. You can read more about the different features on the <a href="/study/philosophy">help page</a>.</c.P>
+          <c.P>The course has been designed to be followed in a linear way. It is separated into several Seasons, each Season including about 10-12 Episodes (or chapters). In every episode, students learn a limited number of new characters, words, sentence patterns and pronunciation, while being sure that previously learnt elements are constantly reviewed and reused.</c.P>
+          <c.P>Currently, only Season 1 is available. Season 2 will be released around late May 2018. Every season is available for purchase for a fixed $19 price. The first three episodes of every season are available for free.</c.P>
+          <c.P>The team is currently based in Stockholm and mainly composed of two people: Johan Björkstén (author) and Tim Astier (developer). If you want to contact us, you can write an email at <i>hello@chinese-me.com</i> or find us on <a href="https://twitter.com/ChineseMeHQ">Twitter</a>.</c.P>
+          <c.P>If you want to be get latest news about the project, you're can subscribe to our newsletter on the <a href="https://www.chinese-me.com">homepage</a>.</c.P>
         </c.Page>
       </ScrollableWrapper>
     );

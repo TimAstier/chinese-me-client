866381dcba5e51a3106bbb2e4abfa815377e9a36
Tim <timothee.astier@essec.edu>
2018-06-04 10:47:17 +0200

    Add prerender-io

--- server/index.js
+++ server/index.js
@@ -4,9 +4,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Setup Prerender.io
+app.use(require('prerender-node').set('prerenderToken', 'OGj5lmcxQRskmWMcLP5Y'));
+
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
-
 // TODO: Manage API requests here
 // Answer API requests.
 // app.get('/api', function (req, res) {
